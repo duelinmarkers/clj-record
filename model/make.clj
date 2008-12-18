f0b032283d9a034bccfb9679260ce9fcb20a8c0d
John D. Hume <duelin.markers@gmail.com>
2008-12-17 23:06:51 -0500

    clean-up

--- model/make.clj
+++ model/make.clj
@@ -4,15 +4,15 @@
 
 (defn find-record [id]
   (sql/with-connection db
-    (sql/with-results rows (str "select * from make where id =" id) 
+    (sql/with-results rows (str "select * from make where id =" id)
       (merge {} (first rows)))))
 
 (defn create [attributes]
-  (let [key-vector (keys attributes) 
-        val-vector (map attributes key-vector)]
-    (sql/with-connection db
-      (let 
-        [id (sql/transaction
-              (sql/insert-values :make key-vector val-vector)
-              (sql/with-results rows "VALUES IDENTITY_VAL_LOCAL()" (:1 (first rows))))]
-        (find-record id)))))
+  (sql/with-connection db
+    (let
+      [key-vector (keys attributes)
+       val-vector (map attributes key-vector)
+       id (sql/transaction
+            (sql/insert-values :make key-vector val-vector)
+            (sql/with-results rows "VALUES IDENTITY_VAL_LOCAL()" (:1 (first rows))))]
+      (find-record id))))
