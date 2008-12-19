68339bd83762c4a97ccf3dc7c59ef7bc437ef414
John D. Hume <duelin.markers@gmail.com>
2008-12-19 11:06:10 -0500

    Extract clj_record/core to make my macro work less messy

---
+++ clj_record/core.clj
@@ -0,0 +1,21 @@
+(ns clj_record.core
+  (:require [clojure.contrib.sql :as sql]))
+
+
+(defn table-name [model-name]
+  (if (keyword? model-name) (.getName model-name) model-name))
+
+(defn find-record [model-name id]
+  (sql/with-connection db
+    (sql/with-results rows (format "select * from %s where id = %s" (table-name model-name) id)
+      (merge {} (first rows)))))
+
+(defn create [model-name attributes]
+  (sql/with-connection db
+    (let
+      [key-vector (keys attributes)
+       val-vector (map attributes key-vector)
+       id (sql/transaction
+            (sql/insert-values (table-name model-name) key-vector val-vector)
+            (sql/with-results rows "VALUES IDENTITY_VAL_LOCAL()" (:1 (first rows))))]
+      (find-record model-name id))))
--- model/manufacturer.clj
+++ model/manufacturer.clj
@@ -1,18 +1,9 @@
 (ns model.manufacturer
-  (:require [clojure.contrib.sql :as sql]))
+  (:require [clj_record.core :as cljrec]))
 
 
 (defn find-record [id]
-  (sql/with-connection db
-    (sql/with-results rows (str "select * from manufacturer where id =" id)
-      (merge {} (first rows)))))
+  (cljrec/find-record :manufacturer id))
 
 (defn create [attributes]
-  (sql/with-connection db
-    (let
-      [key-vector (keys attributes)
-       val-vector (map attributes key-vector)
-       id (sql/transaction
-            (sql/insert-values :manufacturer key-vector val-vector)
-            (sql/with-results rows "VALUES IDENTITY_VAL_LOCAL()" (:1 (first rows))))]
-      (find-record id))))
+  (cljrec/create :manufacturer attributes))
