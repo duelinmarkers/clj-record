51d1a78ca3d258597cbc6aff16d5a5fba22c1c48
John D. Hume <duelin.markers@gmail.com>
2008-12-28 15:43:04 -0500

    Remove good? from manufacturer as it was just noise.

--- clj_record/test/model/manufacturer.clj
+++ clj_record/test/model/manufacturer.clj
@@ -5,12 +5,3 @@
 
 (cljrec/init-model
   (has-many products))
-
-(defn good? 
-  ([manufacturer] 
-    (and (:grade manufacturer) (>= (:grade manufacturer) 90)))
-  {:test (fn []
-    (are (good? {:grade _1})
-      90 90.0 91)
-    (are (not (good? {:grade _1}))
-      89 80 nil))})
