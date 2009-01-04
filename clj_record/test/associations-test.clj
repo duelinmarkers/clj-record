2824861a8e2baa7bcb9dfd312f602d5be74b5b8a
John D. Hume <duelin.markers@gmail.com>
2009-01-04 18:48:39 -0500

    Add a (commented) test for eager fetching. Not sure that's how I want it to look.

--- clj_record/test/associations-test.clj
+++ clj_record/test/associations-test.clj
@@ -17,8 +17,7 @@
         s3000xi (product/create {:name "S-3000xi" :manufacturer_id (:id humedai)})
         s3000xl (product/create {:name "S-3000xl" :manufacturer_id (:id humedai)})]
     (is (= [s3000xi s3000xl] (manufacturer/find-products humedai)))
-    (product/destroy-record s3000xi)
-    (product/destroy-record s3000xl)
+    (manufacturer/destroy-products humedai)
     (manufacturer/destroy-record humedai)))
 
 (deftest has-many-creates-a-destroy-function
@@ -28,3 +27,23 @@
     (manufacturer/destroy-products humedai)
     (is (empty? (manufacturer/find-products humedai)))
     (manufacturer/destroy-record humedai)))
+
+(comment
+(deftest find-records-can-do-eager-fetching-of-has-many-association
+  (let [manu1 (manufacturer/create {:name "manu1" :grade 99})
+        prod1 (product/create {:name "prod1" :manufacturer_id (:id manu1)})
+        prod2 (product/create {:name "prod2" :manufacturer_id (:id manu1)})
+        manu2 (manufacturer/create {:name "manu2" :grade 99})
+        prod3 (product/create {:name "prod3" :manufacturer_id (:id manu2)})
+        prod4 (product/create {:name "prod4" :manufacturer_id (:id manu2)})]
+    (let [[eager-manu1 eager-manu2] (manufacturer/find-records {:grade 99} {:include "products"})]
+      (are (= _1 _2)
+        "manu1" (:name eager-manu1)
+        "manu2" (:name eager-manu2)
+        [prod1 prod2] (:products eager-manu1)
+        [prod3 prod4] (:products eager-manu2)))
+    (manufacturer/destroy-products manu1)
+    (manufacturer/destroy-products manu2)
+    (manufacturer/destroy-record manu1)
+    (manufacturer/destroy-record manu2)))
+)
