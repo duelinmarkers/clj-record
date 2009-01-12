61e4a64617c0f95ba638b475f88b147247b106a7
John D. Hume <duelin.markers@gmail.com>
2009-01-12 00:50:00 -0500

    Name some tests now that test-is requires it.

--- clj_record/test/util-test.clj
+++ clj_record/test/util-test.clj
@@ -4,10 +4,10 @@
   (:use clojure.contrib.test-is))
 
 
-(deftest nil (are (= _1 (util/singularize _2))
+(deftest singularize (are (= _1 (util/singularize _2))
     "foo" "foos"
     "baby" "babies"))
 
-(deftest nil (are (= _1 (util/pluralize _2))
+(deftest pluralize (are (= _1 (util/pluralize _2))
     "foos" "foo"
     "babies" "baby"))
