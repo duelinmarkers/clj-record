f67dddfa2163fee1ff80dd0dfbbda8c90520beee
John D. Hume <duelin.markers@gmail.com>
2009-01-19 09:25:43 -0500

    Cleanup defdbtest a bit.

--- clj_record/test/test_helper.clj
+++ clj_record/test/test_helper.clj
@@ -1,13 +1,13 @@
 (ns clj-record.test.test-helper
+  (:require [clj-record.core :as core])
   (:use clojure.contrib.test-is))
 
 
 (defmacro defdbtest [name & body]
   `(deftest ~name
     (io! "DB test running. No STM allowed."
-      (clojure.contrib.sql/with-connection clj-record.config/db
-        (clojure.contrib.sql/transaction
-          (try
-            ~@body
-            (finally
-              (clojure.contrib.sql/set-rollback-only))))))))
+      (core/transaction
+        (try
+          ~@body
+          (finally
+            (clojure.contrib.sql/set-rollback-only)))))))
