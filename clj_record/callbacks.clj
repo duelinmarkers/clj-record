89909f1fe9cb67d2e0cde4948a22d346e3d13531
John D. Hume <duelin.markers@gmail.com>
2009-01-19 15:10:41 -0500

    Cleanup.

--- clj_record/callbacks.clj
+++ clj_record/callbacks.clj
@@ -11,12 +11,9 @@
       (set-model-metadata-for model-name :callbacks
         (merge-with concat callbacks {hook [func]})))))
 
-(defn callback-functions [model-name hooks]
-  (let [callbacks (model-metadata-for model-name :callbacks)]
-    (reduce #(concat %1 (%2 callbacks)) [] hooks)))
-
 (defn run-callbacks [record model-name & hooks]
-  (loop [r record
-         funcs (callback-functions model-name hooks)]
-    (if (empty? funcs) r
-      (recur ((first funcs) r) (rest funcs)))))
+  (let [callbacks (model-metadata-for model-name :callbacks)]
+    (loop [r record
+           funcs (reduce #(concat %1 (%2 callbacks)) [] hooks)]
+      (if (empty? funcs) r
+        (recur ((first funcs) r) (rest funcs))))))
