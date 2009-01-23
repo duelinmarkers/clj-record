35731ea141f4793532b598a4fc9804c7ff8aad1e
John D. Hume <duelin.markers@gmail.com>
2009-01-23 14:43:27 -0500

    Documentation.

--- clj_record/validation/built_ins.clj
+++ clj_record/validation/built_ins.clj
@@ -1,10 +1,14 @@
 (ns clj-record.validation.built-ins)
 
 
-(defn match [pattern]
+(defn match
+  "Returns a validation function matching value against pattern."
+  [pattern]
   #(re-find pattern (str %)))
 
-(defn non-match [pattern]
+(defn non-match
+  "Returns a validation function ensuring value doesn't match pattern."
+  [pattern]
   (complement (match pattern)))
 
 (def numeric? (non-match #"\D"))
