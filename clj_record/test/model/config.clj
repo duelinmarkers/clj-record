8d13a15eee5bdca183ac5a1d110a2c413e390f64
vishnu <vishnu13@gmail.com>
2009-02-08 13:42:12 -0600

    Added support for postgresql. Minor changes related to id-query-for. Added one new test.

--- clj_record/test/model/config.clj
+++ clj_record/test/model/config.clj
@@ -1,7 +1,14 @@
 (ns clj-record.test.model.config)
 
-
+(comment
 (def db {:classname "org.apache.derby.jdbc.EmbeddedDriver"
          :subprotocol "derby"
          :subname "/tmp/clj-record.test.db"
          :create true})
+)
+
+(def db {:classname "org.postgresql.Driver" 
+         :subprotocol "postgresql" 
+         :subname "//localhost:5433/coffeemug"
+         :user "enter-user-name-here"
+         :password ""})
