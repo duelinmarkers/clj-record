6e534270a9f88eddeb6865c24a626aee656a747f
Tunde Ashafa <tunde.ashafa@gmail.com>
2009-02-12 18:26:03 -0500

    Setting default config back to derby.

--- clj_record/test/model/config.clj
+++ clj_record/test/model/config.clj
@@ -1,10 +1,10 @@
 (ns clj-record.test.model.config)
 
-(comment
-  (def db {:classname "org.apache.derby.jdbc.EmbeddedDriver"
-	   :subprotocol "derby"
-	   :subname "/tmp/clj-record.test.db"
-	   :create true}))
+(comment)
+(def db {:classname "org.apache.derby.jdbc.EmbeddedDriver"
+	 :subprotocol "derby"
+	 :subname "/tmp/clj-record.test.db"
+	 :create true})
 
 (comment
   (def db {:classname "org.postgresql.Driver" 
@@ -13,9 +13,9 @@
            :user "user"
            :password "pass"}))
 
-(comment)
-(def db {:classname "com.mysql.jdbc.Driver" 
-	 :subprotocol "mysql" 
-         :user "root" 
-         :password "root" 
-         :subname "//localhost/test"})
+(comment
+  (def db {:classname "com.mysql.jdbc.Driver" 
+	   :subprotocol "mysql" 
+	   :user "root" 
+	   :password "root" 
+	   :subname "//localhost/test"}))
