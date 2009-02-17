9ad0a7d43041b5aa8f587985c434de02eb435194
Tunde Ashafa <tunde.ashafa@gmail.com>
2009-02-16 21:58:56 -0500

    Tidy up indentation and clean up of generate-predicate-function.

--- clj_record/query.clj
+++ clj_record/query.clj
@@ -2,21 +2,21 @@
   (:require [clojure.contrib.str-utils :as str-utils]))
 
 
-(defmacro generate-predicate [predicate join-predicate-params-with values]
-  `(fn [attribute#]
-     (let [[clause# params#] 
-	   [(str-utils/str-join ~join-predicate-params-with 
-	     (reduce
-	      (fn [predicate-params# value#]
-		(conj predicate-params# (if (nil? value#) "NULL" "?"))) [] ~values))
-	    (filter (complement nil?) ~values)]]
-       [(format (str "%s " ~predicate) (name attribute#) clause#) params#]))) 
+(defn- generate-predicate-fn [predicate join-predicate-params-with values]
+  (fn [attribute]
+     (let [[clause params] 
+	   [(str-utils/str-join join-predicate-params-with
+             (reduce
+               (fn [predicate-params value]
+                 (conj predicate-params (if (nil? value) "NULL" "?"))) [] values))
+            (filter (complement nil?) values)]]
+       [(format (str "%s " predicate) (name attribute) clause) params]))) 
 
 (defn between [value1 value2]
-  (generate-predicate "BETWEEN %s" " AND " [value1 value2]))
+  (generate-predicate-fn "BETWEEN %s" " AND " [value1 value2]))
 
 (defn in [& values]
-  (generate-predicate "IN (%s)" ", " values))
+  (generate-predicate-fn "IN (%s)" ", " values))
 
 (defn like [value]
-  (generate-predicate "LIKE %s" "" [value]))
+  (generate-predicate-fn "LIKE %s" "" [value]))
