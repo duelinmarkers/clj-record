ab2624134cdb611742ba9f745ec15c1b3f086f51
John D. Hume <duelin.markers@gmail.com>
2009-02-25 00:04:26 -0500

    More deobfuscation of query operator definitions.

--- clj_record/query.clj
+++ clj_record/query.clj
@@ -2,32 +2,29 @@
   (:require [clojure.contrib.str-utils :as str-utils]))
 
 
-(defmacro query-operator
-  ([operator-format]               (query-operator operator-format '[value]))
-  ([operator-format args]          (query-operator operator-format args nil))
-  ([operator-format args join-with]
-    (let [params (if (= "&" (name (first args))) (symbol (name (second args))) args)]
-      `(fn ~args
-        (fn [attribute#]
-          (let [clause-params-vector#
-                  (reduce
-                    (fn [operator-params# value#]
-                      (conj operator-params# (if (nil? value#) (throw (Exception. "A query argument must not be nil.")) "?"))) [] ~params)
-                clause-params#
-                  (if (nil? ~join-with)
-                    (apply str clause-params-vector#)
-                    (str-utils/str-join ~join-with clause-params-vector#))]
-            [(format (str "%s " ~operator-format) (name attribute#) clause-params#) (filter (complement nil?) ~params)]))))))
+(defn- operator-fn
+  ([operator-format values] (operator-fn operator-format values nil))
+  ([operator-format values join-with]
+    (fn [attribute]
+      (let [clause-params-vector
+              (reduce
+                (fn [operator-params value]
+                  (conj operator-params (if (nil? value) (throw (Exception. "A query argument must not be nil.")) "?"))) [] values)
+            clause-params
+              (if (nil? join-with)
+                (apply str clause-params-vector)
+                (str-utils/str-join join-with clause-params-vector))]
+        [(format (str "%s " operator-format) (name attribute) clause-params) (filter (complement nil?) values)]))))
 
-(def equal (query-operator "= %s"))
-(def not-equal (query-operator "<> %s"))
-(def greater-than (query-operator "> %s"))
-(def greater-than-or-equal (query-operator ">= %s"))
-(def less-than (query-operator "< %s"))
-(def less-than-or-equal (query-operator "<= %s"))
-(def like (query-operator "LIKE %s" [pattern]))
-(def not-like (query-operator "NOT LIKE %s" [pattern]))
-(def between (query-operator "BETWEEN %s" [value1 value2] " AND "))
-(def not-between (query-operator "NOT BETWEEN %s" [value1 value2] " AND "))
-(def in (query-operator "IN (%s)" [& values] ", "))
-(def not-in (query-operator "NOT IN (%s)" [& values] ", "))
+(defn equal [value] (operator-fn "= %s" [value]))
+(defn not-equal [value] (operator-fn "<> %s" [value]))
+(defn greater-than  [value] (operator-fn "> %s" [value]))
+(defn greater-than-or-equal [value] (operator-fn ">= %s" [value]))
+(defn less-than [value] (operator-fn "< %s" [value]))
+(defn less-than-or-equal [value] (operator-fn "<= %s" [value]))
+(defn like [pattern] (operator-fn "LIKE %s" [pattern]))
+(defn not-like [pattern] (operator-fn "NOT LIKE %s" [pattern]))
+(defn between [value1 value2] (operator-fn "BETWEEN %s" [value1 value2] " AND "))
+(defn not-between [value1 value2] (operator-fn "NOT BETWEEN %s" [value1 value2] " AND "))
+(defn in [& values] (operator-fn "IN (%s)" values ", "))
+(defn not-in [& values] (operator-fn "NOT IN (%s)" values ", "))
