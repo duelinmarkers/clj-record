17de569e90d924c269b9c96022a6bd2af537e126
John D. Hume <duelin.markers@gmail.com>
2009-03-27 11:33:04 -0400

    Cleanup.

--- src/clj_record/test/main.clj
+++ src/clj_record/test/main.clj
@@ -1,8 +1,8 @@
 (ns clj-record.test.main
   (:require [clojure.contrib.sql :as sql]
-            [clojure.contrib.test-is :as test-is]
-            [clojure.contrib.str-utils :as str-utils]
-            clj-record.test.model.config))
+            [clojure.contrib.test-is :as test-is])
+  (:use clj-record.test.model.config
+        clojure.contrib.str-utils))
 
 
 (defmulti get-id-key-spec :subprotocol)
@@ -19,30 +19,32 @@
 
 (defn create-tables []
   (sql/create-table :manufacturers
-    (get-id-key-spec clj-record.test.model.config/db "manufacturer_pk")
+    (get-id-key-spec db "manufacturer_pk")
     [:name    "VARCHAR(32)" "NOT NULL"]
     [:founded "VARCHAR(4)"]
     [:grade   :int])
   (sql/create-table :productos
-    (get-id-key-spec clj-record.test.model.config/db "product_pk")
+    (get-id-key-spec db "product_pk")
     [:name            "VARCHAR(32)" "NOT NULL"]
     [:price           :int]
     [:manufacturer_id :int "NOT NULL"]))
 
-(def this-file *file*)
+(defn reset-db []
+  (println "resetting" (db :subprotocol))
+  (sql/with-connection db
+    (sql/transaction
+      (drop-tables)
+      (create-tables)))
+  (println "database reset"))
 
 (defn load-and-run
   "Resets the test database, then finds, loads, and runs all the tests."
   []
-  (sql/with-connection clj-record.test.model.config/db
-    (sql/transaction
-      (drop-tables)
-      (create-tables)))
-  (println (str (clj-record.test.model.config/db :subprotocol) " database setup complete."))
+  (reset-db)
   (let [test-files  (for [f (.listFiles (java.io.File. "clj_record/test"))
                           :when (re-find #"test.clj$" (.getPath f))]
                       (re-find #"[^.]+" (.getPath f)))
-        test-namespaces (map #(symbol (str-utils/re-gsub #"/" "." (str-utils/re-gsub #"_" "-" %))) test-files)]
+        test-namespaces (map #(symbol (re-gsub #"/" "." (re-gsub #"_" "-" %))) test-files)]
     (doseq [file test-files]
       (load file))
     (apply test-is/run-tests test-namespaces)))
