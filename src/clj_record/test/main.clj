f590d4a995862321c238eed7d80e021a9ad5e35a
John D. Hume <duelin.markers@gmail.com>
2009-03-27 11:18:36 -0400

    Switch to a different approach, not compiling tests.

--- src/clj_record/test/main.clj
+++ src/clj_record/test/main.clj
@@ -1,5 +1,4 @@
 (ns clj-record.test.main
-  (:gen-class)
   (:require [clojure.contrib.sql :as sql]
             [clojure.contrib.test-is :as test-is]
             [clojure.contrib.str-utils :as str-utils]
@@ -30,17 +29,21 @@
     [:price           :int]
     [:manufacturer_id :int "NOT NULL"]))
 
-(defn -main [test-dirname]
+(def this-file *file*)
+
+(defn load-and-run
+  "Resets the test database, then finds, loads, and runs all the tests."
+  []
   (sql/with-connection clj-record.test.model.config/db
     (sql/transaction
       (drop-tables)
       (create-tables)))
   (println (str (clj-record.test.model.config/db :subprotocol) " database setup complete."))
-  (let [test-files  (for [f (-> test-dirname java.io.File. .listFiles)
+  (let [test-files  (for [f (.listFiles (java.io.File. "clj_record/test"))
                           :when (re-find #"test.clj$" (.getPath f))]
-                      f)
-        test-namespaces (map #(symbol (str "clj-record.test." (str-utils/re-gsub #"_" "-" (re-find #"[^.]+" (.getName %))))) test-files)]
+                      (re-find #"[^.]+" (.getPath f)))
+        test-namespaces (map #(symbol (str-utils/re-gsub #"/" "." (str-utils/re-gsub #"_" "-" %))) test-files)]
     (doseq [file test-files]
-      (load (str-utils/re-sub #"src/" "/" (re-find #"[^.]+" (.getPath file)))))
+      (load file))
     (apply test-is/run-tests test-namespaces)))
 
