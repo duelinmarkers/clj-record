99cf06753a080f69f3c5bc1b57089e67603c953b
John D. Hume <duelin.markers@gmail.com>
2009-09-03 23:13:08 -0400

    Make clj-record.util/id-query-for a multimethod.

--- src/clj_record/util.clj
+++ src/clj_record/util.clj
@@ -16,13 +16,13 @@
       (some #(.endsWith lc %) ["s" "z" "ch" "sh" "x"]) (str lc "es")
       :else (str lc "s"))))
 
-(defn id-query-for [{:keys [subprotocol] :as db-spec} table-name]
-  (cond
-    (= subprotocol "derby")
-      "VALUES IDENTITY_VAL_LOCAL()"
-    (= subprotocol "postgresql")
-      (str "SELECT currval(pg_get_serial_sequence('" table-name "','id'))")
-    (= subprotocol "mysql")
-      "SELECT LAST_INSERT_ID()"
-    :else
-      (throw (Exception. (str "Unrecognized db-spec: " db-spec)))))
+(defmulti  id-query-for :subprotocol)
+(defmethod id-query-for "derby" [_ _]
+  "VALUES IDENTITY_VAL_LOCAL()")
+(defmethod id-query-for "postgresql" [_ table-name]
+  (str "SELECT currval(pg_get_serial_sequence('" table-name "','id'))"))
+(defmethod id-query-for "mysql" [_ _]
+  "SELECT LAST_INSERT_ID()")
+(defmethod id-query-for :default [db-spec _]
+  (throw (Exception. (str "Unrecognized db-spec subprotocol: " db-spec))))
+
