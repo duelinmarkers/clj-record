322fee58f7d66b6bcfb2428f524782b4e231db32
Dimitry Gashinsky <dimitry@gashinsky.com>
2010-01-27 05:46:38 +0800

    leiningen project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj-record "1.0-SNAPSHOT"
+  :description "A pseudo-port of ActiveRecord to the Clojure programming language"
+  :url "http://github.com/duelinmarkers/clj-record"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies [[swank-clojure/swank-clojure "1.1.0"]
+                     [org.apache.derby/derby "10.5.3.0_1"]])
