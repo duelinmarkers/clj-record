d367b3d147bef750ca91f616475c825dbab57019
Jim Menard <jim.menard@kaplan.com>
2010-06-17 17:25:31 -0400

    Upgrade to Swank 1.2.1 in project.clj.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/duelinmarkers/clj-record"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[swank-clojure/swank-clojure "1.1.0"]
+  :dev-dependencies [[swank-clojure/swank-clojure "1.2.1"]
                      [org.apache.derby/derby "10.5.3.0_1"]])
 
 
