4d9a671c717eca5f4fd716feb26c9b11ee09c7ac
Yair Iny <yair.iny@gmail.com>
2011-01-10 10:20:25 +1100

    Added basic DB2 support

--- test/clj_record/test_model/config.clj
+++ test/clj_record/test_model/config.clj
@@ -14,8 +14,15 @@
            :password "pass"}))
 
 (comment
-  (def db {:classname "com.mysql.jdbc.Driver" 
-	   :subprotocol "mysql" 
+  (def db {:classname "com.mysql.jdbc.Driver"
+	   :subprotocol "mysql"
 	   :user "root" 
 	   :password "root" 
 	   :subname "//localhost/test"}))
+
+(comment
+  (def db {:classname "com.ibm.db2.jcc.DB2Driver"
+	   :subprotocol "db2"
+	   :user "db2"
+	   :password "db2"
+	   :subname "//localhost:50000/clojure:currentSchema=CLJRECORD;"}))
