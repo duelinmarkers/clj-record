2d7702458d21582c934d4d96a512142061edbb35
John Hume <duelin.markers@gmail.com>
2011-10-29 09:50:30 -0400

    Remove unused dependency on org.clojure.contrib/seq 1.3.0-alpha4.

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "A pseudo-port of ActiveRecord to the Clojure programming language"
   :url "http://github.com/duelinmarkers/clj-record"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure.contrib/seq "1.3.0-alpha4"]
                  [org.clojure/java.jdbc "0.0.6"]]
   :dev-dependencies [[lein-clojars "0.6.0"]
                      [swank-clojure/swank-clojure "1.2.1"]
