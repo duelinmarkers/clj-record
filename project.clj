b05dff27cf9814440ac907a3ead665d083c2e42c
John Hume <duelin.markers@gmail.com>
2012-07-11 07:39:40 -0400

    Up clojure.java.jdbc dep to 0.1.4

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A pseudo-port of ActiveRecord to the Clojure programming language"
   :url "http://github.com/duelinmarkers/clj-record"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/java.jdbc "0.1.1"]]
+                 [org.clojure/java.jdbc "0.1.4"]]
   :dev-dependencies [[lein-clojars "0.6.0"]
                      [swank-clojure/swank-clojure "1.2.1"]
                      [mysql/mysql-connector-java "5.1.17"]
